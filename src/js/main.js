ca2a24490eb4a61382383ad5ae3b1d419c0fe927
BigPignouf <fnockle@gmail.com>
2019-02-26 21:50:30 +0100

    adding a reset button for the filter

--- src/js/main.js
+++ src/js/main.js
@@ -27,6 +27,36 @@ function fetchComics(idCat){
   }).catch(e => console.warn(e)); //gestion de potentielles erreurs
 }
 
+//Réinitialise le fitlre de catégories en allant rechercher toutes les oeuvres
+function resetFilterComics(){
+  fetch('resetFilterComics.php').then(r=>r.json()).then(r=>{
+
+    let html = "";
+
+    r.forEach((v, k) => {
+
+      html += `<div class="individualComics">
+                <article>
+                  <h3>${v.comics_name}</h3>
+                  <img src="img/comics/${v.picture}" alt="${v.comics_name}" height="181" width="125">
+                  <p>${v.categorie}</p>
+                </article>
+                <article class="hidden">
+                  <h3>${v.author_name}</h3>
+                  <p>${v.comics_description}</p>
+                  <p>Vous pourriez aimer : ${v.comics_alternative}</p>
+                </article>
+              </div>`;
+
+    });
+
+    document.getElementById('comics__list').innerHTML = html;
+
+  }).catch(e => console.warn(e)); //gestion de potentielles erreurs
+  //Remet le select sur sa valeur par default
+  document.querySelector("#categoryFilter").selectedIndex = 0;
+}
+
 function fetchMangas(idCat){
   fetch('fetchMangas.php?idCat='+idCat).then(r=>r.json()).then(r=>{
 
@@ -54,6 +84,34 @@ function fetchMangas(idCat){
   }).catch(e => console.warn(e));
 }
 
+function resetFilterManga(){
+  fetch('resetFilterManga.php').then(r=>r.json()).then(r=>{
+
+    let html = "";
+
+    r.forEach((v, k) => {
+
+      html += `<div class="individualManga">
+                <article>
+                  <h3>${v.comics_name}</h3>
+                  <img src="img/comics/${v.picture}" alt="${v.comics_name}" height="181" width="125">
+                  <p>${v.categorie}</p>
+                </article>
+                <article class="hidden">
+                  <h3>${v.author_name}</h3>
+                  <p>${v.comics_description}</p>
+                  <p>Vous pourriez aimer : ${v.comics_alternative}</p>
+                </article>
+              </div>`;
+
+    });
+
+    document.getElementById('mangas__list').innerHTML = html;
+
+  }).catch(e => console.warn(e));
+  document.querySelector("#categoryFilter").selectedIndex = 0;
+}
+
 function fetchBds(idCat){
   fetch('fetchBds.php?idCat='+idCat).then(r=>r.json()).then(r=>{
 
@@ -79,4 +137,32 @@ function fetchBds(idCat){
     document.getElementById('bds__list').innerHTML = html;
 
   }).catch(e => console.warn(e));
+}
+
+function resetFilterBD(){
+  fetch('resetFilterBD.php').then(r=>r.json()).then(r=>{
+
+    let html = "";
+
+    r.forEach((v, k) => {
+
+      html += `<div class="individualBD">
+                <article>
+                  <h3>${v.comics_name}</h3>
+                  <img src="img/comics/${v.picture}" alt="${v.comics_name}" height="181" width="125">
+                  <p>${v.categorie}</p>
+                </article>
+                <article class="hidden">
+                  <h3>${v.author_name}</h3>
+                  <p>${v.comics_description}</p>
+                  <p>Vous pourriez aimer : ${v.comics_alternative}</p>
+                </article>
+              </div>`;
+
+    });
+
+    document.getElementById('bds__list').innerHTML = html;
+
+  }).catch(e => console.warn(e));
+  document.querySelector("#categoryFilter").selectedIndex = 0;
 }
---
+++ src/resetFilterManga.php
@@ -0,0 +1,15 @@
+<?php include 'includes/bdd_connection.php';
+
+$query = $pdo->prepare('
+  SELECT id_cat, id_comics, id_author, id_type, picture, comics_name, author_name, comics_description, comics_alternative, cat_name, is_active, GROUP_CONCAT(cat_name SEPARATOR ", ") AS categorie
+  FROM category_comics
+  JOIN comics USING (id_comics)
+  JOIN category USING (id_cat)
+  JOIN author USING (id_author)
+  WHERE id_type = 1
+  GROUP BY id_comics, id_author, id_type, picture, comics_name, comics_description, comics_alternative, is_active
+');
+$query->execute();
+$mangas = $query->fetchAll();
+
+echo json_encode($mangas);
